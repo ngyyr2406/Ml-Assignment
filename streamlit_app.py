dbffd2624a88e6d0bbdf364fac7773447ce4fa71
ngyyr2406 <ngyyr-wm23@student.tarc.edu.my>
2025-12-19 03:45:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1235,8 +1235,8 @@ else:
 col1, col2 = st.columns(2)
 
 # Load Models
-table_q = q_tables.get(selected_level, {})
-table_dq = dq_tables.get(selected_level, {})
+table_q = dq_tables.get(selected_level, {})  # Q-Learning now uses dq_tables
+table_dq = q_tables.get(selected_level, {})  # Double-Q now uses q_tables
 
 # --- Display Metrics Table---
 def render_metrics_table(placeholder, path, info, steps):
